aa3ca25e5ddb751a100d251204a818f8e2e56df2
Anuragkushwah21 <anuragkushwah5100@gmail.com>
2026-05-27 16:24:34 +0530

    fix url in frontend

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(
   {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://shecan-foundation-form-five.vercel.app/"],
     credentials:true,
   }
 ));
