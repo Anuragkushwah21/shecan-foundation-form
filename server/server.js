694c95c770eb37863c7bd4c738f05fc6af576921
Anuragkushwah21 <anuragkushwah5100@gmail.com>
2026-05-27 16:26:12 +0530

    update backend

--- server/server.js
+++ server/server.js
@@ -16,16 +16,9 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(cors(
-//   {
-//     origin: "https://hhhhhh.netlify.app",
-//     credentials:true,
-//   }
-// ));
-
 app.use(cors(
   {
-    origin: ["http://localhost:5173","https://shecan-foundation-form-five.vercel.app/"],
+    origin: "https://shecan-foundation-form-five.vercel.app/",
     credentials:true,
   }
 ));
